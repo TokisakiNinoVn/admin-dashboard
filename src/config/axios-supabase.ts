696d8b11f9c803a1709204b87621ae1b4f81f5e9
TokisakiNinoDev <tokisaki.nino.devstudio@gmail.com>
2026-09-11 16:34:55 +0700

    feat: add SampleCard5 component for project review display

--- src/config/axios-supabase.ts
+++
@@ -1,9 +0,0 @@
-const supabaseUrl: string = process.env.NEXT_PUBLIC_API_SUPABASE!;
-const supabaseAnonKey: string = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-import { createClient } from '@supabase/supabase-js';
-console.log('supabaseUrl', supabaseUrl);
-console.log('supabaseAnonKey', supabaseAnonKey);
-
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
-export default supabase;
